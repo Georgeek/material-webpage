43244a7a6c9f59fb99d91d8ce7eacb4d6d9ae6b2
georgeek <egormad@gmail.com>
2017-08-23 17:12:22 +0800

    first commit 3

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app" class="mdl-layout mdl-js-layout is-upgraded">
     <header class="mdl-layout__header">
+      <div class="mdl-layout__tab-bar-container mdl-layout--large-screen-only"></div>
       <div class="mdl-layout__drawer-button mdl-layout--small-screen-only">
         <!-- button for mobile nav -->
         <i class="material-icons">menu</i>
@@ -14,17 +15,20 @@
           </div>
           <div class="person__data">
             <h3>Мадьяров Егор</h3>
-            <button class="mdl-button mdl-js-button mdl-button--raised">
-              Резюме
-            </button>
-            <button class="mdl-button mdl-js-button mdl-button--raised">
-              Резюме 1
-            </button>
+            <div class="person__button-group">
+              <button class="mdl-button mdl-js-button mdl-button--raised">
+                Резюме
+              </button>
+              <button class="mdl-button mdl-js-button mdl-button--raised">
+                Резюме 1
+              </button>
+            </div>  
           </div>
         </div>
       </div>
       <div class="mdl-layout__header-row mdl-layout--small-screen-only"></div>
 
+      <div class="mdl-layout__tab-bar-container mdl-layout--large-screen-only"></div>
       <div class="mdl-layout__tab-bar-container mdl-layout--large-screen-only">
         <div class="mdl-layout__tab-bar mdl-ripple-effect mdl-color--primary-dark">
           <a href="" class="mdl-layout__tab is-active">
@@ -45,7 +49,8 @@
         </div>
       </div>
     </header>
-
+    <div class="mdl-layout__header-row mdl-layout--small-screen-only transparent"></div>
+    <div class="mdl-layout__header-row mdl-layout--small-screen-only transparent"></div>
     <!-- mobile nav -->
     <div class="mdl-layout__drawer mdl-layout--small-screen-only">
       <nav class="mdl-navigation mdl-typography--body-1-force-preferred-font">
@@ -55,20 +60,96 @@
         <a href="" class="mdl-navigation__link"><i class="material-icons">receipt</i><span>Контакты</span></a>
       </nav>
     </div>
-
+    
     <main class="mdl-layout__content">
-      <div class="mdl-card">
-        <div class="mdl-card__title">
-          <h2 class="mdl-card__title-text">Title</h2>
+      <div class="mdl-grid">
+        <div class="mdl-cell mdl-cell--4-col mdl-cell--12-col-tablet mdl-cell--12-col-phone">
+          <div class="mdl-grid">
+            <div class="mdl-cell mdl-cell--12-col mdl-card mdl-shadow--4dp">
+              <div class="mdl-card__title">
+                <i class="material-icons material-icons--info">public</i>
+                <h5>Title 1n</h5>
+              </div>
+              <div class="mdl-card__supporting-text">Some awesome text</div>
+              <div class="mdl-card__actions mdl-card--border">
+                <a href="" class="mdl-button mdl-button-colored mdl-js-button mdl-js-ripple-effect">Get Started</a>
+              </div>
+              <div class="mdl-card__menu">
+                <button class="mdl-button mdl-button--icon mdl-js-button mdl-ripple-effect">
+                  <i class="material-icons">share</i>
+                </button>
+              </div>
+            </div>
+            <div class="mdl-cell mdl-cell--12-col mdl-card mdl-shadow--4dp">
+              <div class="mdl-card__title">
+                <i class="material-icons material-icons--info">public</i>
+                <h5>Title 1n</h5>
+              </div>
+              <div class="mdl-card__supporting-text">Some awesome text</div>
+              <div class="mdl-card__actions mdl-card--border">
+                <a href="" class="mdl-button mdl-button-colored mdl-js-button mdl-js-ripple-effect">Get Started</a>
+              </div>
+              <div class="mdl-card__menu">
+                <button class="mdl-button mdl-button--icon mdl-js-button mdl-ripple-effect">
+                  <i class="material-icons">share</i>
+                </button>
+              </div>
+            </div>
+            <div class="mdl-cell mdl-cell--12-col mdl-card mdl-shadow--4dp">
+              <div class="mdl-card__title">
+                <i class="material-icons material-icons--info">public</i>
+                <h5>Title 1n</h5>
+              </div>
+              <div class="mdl-card__supporting-text">Some awesome text</div>
+              <div class="mdl-card__actions mdl-card--border">
+                <a href="" class="mdl-button mdl-button-colored mdl-js-button mdl-js-ripple-effect">Get Started</a>
+              </div>
+              <div class="mdl-card__menu">
+                <button class="mdl-button mdl-button--icon mdl-js-button mdl-ripple-effect">
+                  <i class="material-icons">share</i>
+                </button>
+              </div>
+            </div>
+            <div class="mdl-cell mdl-cell--12-col mdl-card mdl-shadow--4dp">
+              <div class="mdl-card__title">
+                <i class="material-icons material-icons--info">public</i>
+                <h5>Title 1n</h5>
+              </div>
+              <div class="mdl-card__supporting-text">Some awesome text</div>
+              <div class="mdl-card__actions mdl-card--border">
+                <a href="" class="mdl-button mdl-button-colored mdl-js-button mdl-js-ripple-effect">Get Started</a>
+              </div>
+              <div class="mdl-card__menu">
+                <button class="mdl-button mdl-button--icon mdl-js-button mdl-ripple-effect">
+                  <i class="material-icons">share</i>
+                </button>
+              </div>
+            </div>
+
+          </div>
         </div>
-        <div class="mdl-card__supporting-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sagittis pellentesque lacus eleifendinia...</div>
-        <div class="mdl-card__actions mdl-card--border">
-          <a href="" class="mdl-button mdl-button-colored mdl-js-button mdl-js-ripple-effect">Get Started</a>
-        </div>
-        <div class="mdl-card__menu">
-          <button class="mdl-button mdl-button--icon mdl-js-button mdl-ripple-effect">
-            <i class="material-icons">share</i>
-          </button>
+        <div class="mdl-cell mdl-cell--8-col">
+          <div class="mdl-grid">
+            <div class="mdl-cell mdl-cell--12-col mdl-card mdl-shadow--2dp">
+              <div class="mdl-card__title">
+                <h2 class="mdl-card__title-text">Welcome</h2>
+              </div>
+              <div class="mdl-card__supporting-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Mauris sagittis pellentesque lacus eleifend lacinia...
+              </div>
+              <div class="mdl-card__actions mdl-card--border">
+                <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+                  Get Started
+                </a>
+              </div>
+              <div class="mdl-card__menu">
+                <button class="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect">
+                  <i class="material-icons">keyboard_arrow_down</i>
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </main>
@@ -85,39 +166,62 @@
 <style src="material-design-lite/material.min.css"></style>
 <style>
   @import url('https://fonts.googleapis.com/icon?family=Material+Icons');
+  @import url('https://fonts.googleapis.com/css?family=Roboto');
 </style>
 
 <style lang="sass">
   body
-    font-family: 'Material Icons', sans, sans serif
+    font-family: 'Roboto', sans-serif
   h3
     margin: 33px 0 15px
-  .mdl-layout__header
-    background: url(assets/bg.jpg) center no-repeat
-    background-size: cover
-    border-top: 4px solid #303f9f
-  .mdl-layout__header-row
-    background: rgba(101, 115, 242, .3)
 
-  .mdl-layout__tab-bar-container
-    background: rgba(101, 115, 242, .3)
+  .mdl-card
+    margin: 8px auto
+  .mdl-layout, .mdl-layout__content
     overflow: visible
-    height: 72px
-  .mdl-layout__tab-bar
-    height: 72px
-    padding: 0
-    overflow: visible
-    position: absolute
-    top: 35px
-    right: 20%
-    width: auto
-    box-shadow: 0 2px 2px 0 rgba(0,0,0,.14), 0 3px 1px -2px rgba(0,0,0,.2), 0 1px 5px 0 rgba(0,0,0,.12)
+  .mdl-layout
+    margin: 0 auto
+    max-width: 1280px
+    &__content
+      padding: 21px 21px 0px 21px
+    
+    &__header
+      background: url(assets/bg.jpg) center no-repeat
+      background-size: cover
+      border-top: 4px solid #303f9f
+    &__tab-bar
+      height: 72px
+      padding: 0
+      overflow: visible
+      position: absolute
+      top: 35px
+      right: 13%
+      width: auto
+      box-shadow: 0 2px 2px 0 rgba(0,0,0,.14), 0 3px 1px -2px rgba(0,0,0,.2), 0 1px 5px 0 rgba(0,0,0,.12)
+    &__header-row
+      background: rgba(101, 115, 242, .3)
+    .transparent
+      background: transparent
+    &__tab-bar-container
+      background: rgba(101, 115, 242, .3)
+      overflow: visible
+      height: 72px
+      .mdl-button--icon
+        position: relative
+        top: 20px
+        margin-right: 20px
+    &__tab
+      height: 100%
+      line-height: 25px
+      display: flex
+      flex-direction: column
+      padding: 0 38px
+      i
+        padding-top: 15px
+    &.is-upgraded &__tab.is-active::after
+      background: white
+      height: 5px
 
-    .mdl-button--icon
-      position: relative
-      top: 10px
-      margin-right: 25px
-  
   .person__block
     display: flex
     flex-direction: row
@@ -132,30 +236,27 @@
   .logo-img
     width: 145px
     height: 145px
-    
   .logo-img--photo
     width: 100%
     height: 100%
     border-radius: 50% 
     background: url(assets/photo.jpg) center center no-repeat
     background-size: 150%
-
-  .mdl-layout__tab
-    height: 100%
-    line-height: 25px
-    display: flex
-    flex-direction: column
-    padding: 0 38px
-
-    i
-      padding-top: 15px
-
-  .mdl-layout.is-upgraded .mdl-layout__tab.is-active::after
-    background: white
-    height: 5px
+    box-shadow: 0px 11px 13px 1px rgba(0,0,0,0.14)
 
   main
-    background: pink
+    background: #fff
+    height: 500vh
+  .material-icons--info
+    border: 7px solid #e0e0e0
+    border-radius: 50px
+    background: #e0e0e0
+    color: white
+    position: relative
+    top: 3px
+    margin-right: 10px
+    font-size: 28px
+
   @media screen and (max-width: 1024px)
     .mdl-layout__header
       display: block
@@ -172,7 +273,28 @@
 
       h3
         color: black
-        margin-left: 0
+        margin: 15px 16px 19px
+    .person__data
+      margin-left: 0
+    .person__button-group
+      display: flex
+      justify-content: space-between
+      .mdl-button
+        margin-right: 0
+        &--raised
+          background: transparent
+          color: #bfbfbf
+          box-shadow: none
+    .logo-img--photo
+      box-shadow: 0px 11px 13px 1px rgba(0,0,0,0.14)
+    main .mdl-grid
+      flex-direction: column
+      align-items: center
+    .mdl-layout__content
+      padding: 21px 0 0 0
+    .mdl-cell--4-col
+      width: calc(100% - 16px)
+
     
 </style>
 
